c52e30116875a7989a596521dda9fa128482a558
camaraajcv <123605493+camaraajcv@users.noreply.github.com>
2024-04-23 23:25:31 +0000

    sdfsdfd

--- Hello.py
+++ Hello.py
@@ -5,15 +5,8 @@ def main():
     # URL da imagem
     image_url = "logoSIGRH_Cabecalho.png"
 
-    # Código HTML e CSS para centralizar a imagem
-    centered_html = f"""
-    <div style="display: flex; justify-content: center;">
-        <img src="{image_url}" alt="Logo SIGPP" style="width: 50%;">
-    </div>
-    """
-
-    # Exibindo a imagem centralizada
-    st.markdown(centered_html, unsafe_allow_html=True)
+    # Exibindo a imagem
+    st.image(image_url, caption='',width=400)
     
     # Centralizar o texto abaixo da imagem
     st.markdown("<h1 style='text-align: center; font-size: 1.5em;'>DIRETORIA DE ADMINISTRAÇÃO DA AERONÁUTICA</h1>", unsafe_allow_html=True)
